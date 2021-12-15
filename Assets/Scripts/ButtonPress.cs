6949a69da99a8b476164e035bea0bf0b0a17ac79
Aki Järvinen <aki@gamefutures.com>
2021-12-15 15:54:41 +0000

    wallbutton prefabs

--- Assets/Scripts/ButtonPress.cs
+++ Assets/Scripts/ButtonPress.cs
@@ -5,117 +5,73 @@ using System;
 
 public class ButtonPress : MonoBehaviour
 {
-    public bool lockX;
-    public bool lockY;
-    public bool lockZ;
+    private Animator anim;
+    private bool isButtonDown;
+    private bool isButtonIdle;
+    //private bool isButtonActive;
+    
 
-    public float speed;
-    public float returnSpeed;
-    private float distanceX = 0;
-    private float distanceY = 0;
-    private float distanceZ = 0;
-
-    [SerializeField]
-    private bool isButtonOut = true;
- 
-
-    // This is the second part of the button which remains in static position but changes color according to press
-    public GameObject indicatorObject;
-    private Collider indicatorCollider;
-
-    private AudioSource audioSource;
-
-   
-    protected Vector3 startPosition;
-    private Vector3 localPos;
-
-    void Start()
+    private void Start()
     {
-        // Remember start position of button
-        startPosition = transform.localPosition;
+        anim = GetComponent<Animator>();
+      
+        isButtonDown = anim.GetBool("isButtonDown");
+        isButtonIdle = anim.GetBool("isButtonIdle");
+        anim.Play("ButtonReset");
 
-        // Use local position instead of global, so button can be rotated in any direction
-        localPos = transform.localPosition;
-        if (lockX) localPos.x = startPosition.x;
-        if (lockY) localPos.y = startPosition.y;
-        if (lockZ) localPos.z = startPosition.z;
-        transform.localPosition = localPos;
-
-        audioSource = GetComponent<AudioSource>();
-        indicatorCollider = indicatorObject.GetComponent<Collider>();
 
     }
 
-    public void AllowPress()
+    public void PressButton()
     {
-       if(isButtonOut)
+        if(isButtonIdle)
         {
-            isButtonOut = false;
-            Debug.Log("press button registered");
-            audioSource.Play();
-        }
-       
+            anim.Play("ButtonDown");
+            anim.SetBool("isButtonDown", true);
+            anim.SetBool("isButtonIdle", false);
+            //anim.SetTrigger("buttonPressed");
+
+        }    
+        
     }
 
     public void ResetButton()
     {
-        if(!isButtonOut )
+        if (isButtonDown)
         {
-            isButtonOut = true;
-            transform.Translate(startPosition);
+            anim.Play("ButtonReset");
+            
+            
+
+            anim.SetBool("isButtonDown", false);
+            anim.SetBool("isButtonIdle", true);
         }
-        
-        
     }
 
-
-    void Update()
+    public void ToggleButton()
     {
+       
 
-
-        //released = false;
-        if (!isButtonOut)
+        if (isButtonDown)
         {
-            //Get distance of button press. Make sure to only have one moving axis.
-            Vector3 allDistances = transform.localPosition + indicatorObject.transform.localPosition + indicatorCollider.bounds.size; // startPosition;
-            if (!lockX)
-            {
-                distanceX = Math.Abs(allDistances.x);
-            }
-
-            if (!lockY)
-            {
-                distanceY = Math.Abs(allDistances.y);
-            }
-
-            if (!lockZ)
-            {
-                distanceZ = Math.Abs(allDistances.z);
-            }
-
-            //if (allDistances.x > transform.localPosition.x)
-            //{
-            transform.Translate(-distanceX * Time.deltaTime * speed, distanceY * Time.deltaTime * speed, distanceZ * Time.deltaTime * speed);
-            //}
+            ResetButton();
+            Debug.Log("Resetting button");
+            
         }
         else
         {
-           // isButtonOut = true;
 
-             if (transform.localPosition.x > startPosition.x)
-             {
-            // Return button to startPosition
-            transform.Translate(startPosition.x * Time.deltaTime * returnSpeed, startPosition.y, startPosition.z);
-             }
+            PressButton();
+            Debug.Log("Pressing button");
         }
+       
         
-            
-            
-        
-        
-        
+    }
 
-    }   
-
+    private void Update()
+    {
+        isButtonDown = anim.GetBool("isButtonDown");
+        isButtonIdle = anim.GetBool("isButtonIdle");
+    }
 
 }
