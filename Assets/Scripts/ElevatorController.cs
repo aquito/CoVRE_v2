6a40dc2220aeb1637d9fd238867e4bb48f25be0b
kerwyndyte <kdyte1997@gmail.com>
2022-01-12 20:10:08 +0000

    Working Lift (NOT NETWORKED)
    Lift is now working minor bug where you might have to reapply the script to the button on loading the project.
    
    .gitignore changed to ignore logs of all kinds.
    
    Potential fix for teleporting through walls, Velocity for interactors is too fast (16), reducing to half that lowers the distance you are able to teleport thusly cannot reach opposite rooms as they are too far out from each other.
    
    Lift Room set to default layer once again to stop teleportation into lift room through stone gate.

--- Assets/Scripts/ElevatorController.cs
+++ Assets/Scripts/ElevatorController.cs
@@ -1,30 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.XR.Interaction.Toolkit;
 public class ElevatorController : MonoBehaviour
 {
 
     public Animator animator;
     public GameObject PlayerObject;
+    public GameObject PositionTransformer;
+    public bool RaiseLiftCalled = false;
+    
 
-    public void OnTriggerEnter(Collider other)
+
+    private void Update()
     {
-        if (other.tag == "Player")
+        if(isPlaying(animator, "RaiseFloor") == true) // Checks if RaiseFloor animation state is running
         {
-            Debug.Log("Player is in the Zone");
-            PlayerObject.transform.parent = gameObject.transform;
-            animator.SetTrigger("PlayerEnterTrigger");            
+            PlayerObject.transform.position = PositionTransformer.transform.position; 
+        }
+        else if(isPlaying(animator, "RaiseFloorCompleted") == true)
+        {
+            RaiseLiftCalled = false;
+            PositionTransformer.transform.parent = PlayerObject.transform;
         }
     }
 
-    public void OnTriggerExit(Collider other)
-    {
-        if (other.tag == "Player")
+    public void RaiseLift() // Is called by the Button Script to start the animation for raising the lift
+    {       
+
+        RaiseLiftCalled = true;
+
+        if (RaiseLiftCalled)
         {
-            Debug.Log("Player has left the zone");
-            PlayerObject.transform.parent = null;
-        }
+            PositionTransformer.transform.parent = gameObject.transform;           
+            animator.SetBool("PlayerPressedButton", true);            
+        }      
+
+        
+    }
+
+    bool isPlaying(Animator anim, string stateName) // Function for checking animation states
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName(stateName) &&
+                anim.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f)
+            return true;
+        else
+            return false;
     }
 
 
